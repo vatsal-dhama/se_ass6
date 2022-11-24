ed4fe6a41a451036d51f4958d430a5fd7549c750
vatsal007 <75558599+vatsal-dhama@users.noreply.github.com>
2022-11-25 00:11:20 +0530

    Update testwrong.py

--- testwrong.py
+++ testwrong.py
@@ -1,1 +1,21 @@
-fd
+#!/usr/bin/python3
+# Test case for adding two numbers
+import unittest
+
+from code import modulo
+
+class TestModulo(unittest.TestCase):
+	def test_4(self):
+		a_4 = 4
+		b_4 = 2
+		result = modulo(a_4, b_4)
+		self.assertEqual(result, 2) 
+
+	def test_5(self):
+		a_5 = 10
+		b_5 = 3
+		result = modulo(a_5, b_5)
+		self.assertEqual(result, 3)
+
+if __name__ == '__main__':
+	unittest.main()
