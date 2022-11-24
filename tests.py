a2bd61d9194c297eb8de157f51d2f6805ae0b4f8
vatsal007 <75558599+vatsal-dhama@users.noreply.github.com>
2022-11-25 00:10:16 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -23,17 +23,5 @@ class TestModulo(unittest.TestCase):
 		result = modulo(a_3, b_3)
 		self.assertEqual(result, 2)
 
-	def test_4(self):
-		a_4 = 4
-		b_4 = 2
-		result = modulo(a_4, b_4)
-		self.assertEqual(result, 2) 
-
-	def test_5(self):
-		a_5 = 10
-		b_5 = 3
-		result = modulo(a_5, b_5)
-		self.assertEqual(result, 3)
-
 if __name__ == '__main__':
 	unittest.main()
